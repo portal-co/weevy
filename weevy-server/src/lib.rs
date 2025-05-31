8868c175bbab86933601ae38ebd7645926bdd39a
Graham Kelly <gkgoat6700@gmail.com>
2025-05-31 18:32:52 +0000

    upgrade and stuff

--- weevy-server/src/lib.rs
+++ weevy-server/src/lib.rs
@@ -1,22 +1,45 @@
 use anyhow::Context as AnyhowContext;
 use axum::{body::Body, extract::Request};
 use base64::Engine;
+use sha3::Digest;
 use swc_atoms::Atom;
 use swc_common::{
     FileName, FilePathMapping, SourceMap, Spanned, SyntaxContext,
     errors::{ColorConfig, Handler},
     sync::Lrc,
 };
-use swc_ecma_ast::{EsVersion, Expr, Ident, IdentName, MemberExpr};
+use swc_ecma_ast::{EsVersion, Expr, Ident, IdentName, MemberExpr, Program};
 use swc_ecma_parser::{Parser, StringInput, Syntax, lexer::Lexer};
 use swc_ecma_visit::VisitMutWith;
 use weevy_swc_core::{default_source_mapper, wevy};
-
-pub async fn server(a: Request) -> anyhow::Result<Body> {
+pub trait Plugin {
+    type State;
+    fn state(&self, a: &Request) -> anyhow::Result<Self::State>;
+    type RewriteState;
+    fn rewrite_state(
+        &self,
+        s: Self::State,
+        body: &[u8],
+        ty: &str,
+    ) -> anyhow::Result<Self::RewriteState>;
+    fn rewrite_js(&self, state: &mut Self::RewriteState, prog: &mut Program) -> anyhow::Result<()>;
+}
+pub async fn server(a: Request, plugin: &impl Plugin) -> anyhow::Result<Body> {
+    let mut state = plugin.state(&a).context("in preparing the plugin")?;
     let q2 = a.uri().query().context("in getting the query")?;
     let q = q2.split("&");
-    let v = match q.clone().find_map(|q| q.strip_prefix("code=")) {
-        Some(a) => base64::engine::general_purpose::STANDARD.decode(a)?,
+    let gid = q
+        .clone()
+        .find_map(|q| q.strip_prefix("gid="))
+        .unwrap_or("MainContent");
+    let (v, ct) = match q.clone().find_map(|q| q.strip_prefix("code=")) {
+        Some(a) => (
+            base64::engine::general_purpose::STANDARD.decode(a)?,
+            q.clone()
+                .find_map(|q| q.strip_prefix("ct="))
+                .context("in getting the content type")?
+                .to_owned(),
+        ),
         None => {
             let exter = q
                 .clone()
@@ -24,55 +47,75 @@ pub async fn server(a: Request) -> anyhow::Result<Body> {
                 .context("in getting the external url or code")?;
             let exter = base64::engine::general_purpose::STANDARD.decode(exter)?;
             let exter = String::from_utf8(exter)?;
-            reqwest::get(exter).await?.bytes().await?.into()
+            let q = reqwest::get(exter).await?;
+            let Some(ct) = q
+                .headers()
+                .get("Content-Type")
+                .and_then(|a| a.to_str().ok())
+            else {
+                return Ok(q.bytes().await?.into());
+            };
+            let ct = ct.to_owned();
+            (q.bytes().await?.into(), ct)
         }
     };
-    let v = String::from_utf8(v)?;
+    let mut state = plugin
+        .rewrite_state(state, &v, ct.as_str())
+        .context("in preparing the plugin for rewriting")?;
     let mut cm = Lrc::new(SourceMap::new(FilePathMapping::default()));
-    // let a =
-    let fm = cm.new_source_file(FileName::Custom("source.js".into()).into(), v);
-    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
-    let lexer = Lexer::new(
-        // We want to parse ecmascript
-        Syntax::Es(Default::default()),
-        // EsVersion defaults to es5
-        EsVersion::latest(),
-        StringInput::from(&*fm),
-        None,
-    );
+    match ct.as_str() {
+        "text/javascript" | "application/javascript" => {
+            let v = String::from_utf8(v)?;
 
-    let mut parser = Parser::new_from(lexer);
+            // let a =
+            let fm = cm.new_source_file(FileName::Custom("source.js".into()).into(), v);
+            let handler =
+                Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+            let lexer = Lexer::new(
+                // We want to parse ecmascript
+                Syntax::Es(Default::default()),
+                // EsVersion defaults to es5
+                EsVersion::latest(),
+                StringInput::from(&*fm),
+                None,
+            );
 
-    for e in parser.take_errors() {
-        e.into_diagnostic(&handler).emit();
+            let mut parser = Parser::new_from(lexer);
+
+            for e in parser.take_errors() {
+                e.into_diagnostic(&handler).emit();
+            }
+
+            let mut prog = parser
+                .parse_program()
+                .map_err(|e| e.into_diagnostic(&handler))
+                .map_err(|e| anyhow::anyhow!("core: {e:?}"))?;
+
+            prog.visit_mut_with(&mut weevy_swc_core::SourceMapper {
+                sm: cm.clone(),
+                data: Default::default(),
+                id: Ident::new(
+                    Atom::new(format!(
+                        "__Weevy_{}_Inject",
+                        base64::engine::general_purpose::STANDARD
+                            .encode(sha3::Sha3_256::digest(q2))
+                    )),
+                    prog.span(),
+                    SyntaxContext::empty(),
+                ),
+                root: SyntaxContext::empty(),
+                source_mapper: default_source_mapper(&prog, SyntaxContext::empty()),
+            });
+            prog.visit_mut_with(&mut weevy_swc_core::Wimple {
+                root: SyntaxContext::empty(),
+                guest_id: Atom::new(gid),
+            });
+            plugin
+                .rewrite_js(&mut state, &mut prog)
+                .context("in rewriting using the plugin")?;
+
+            Ok(Body::new(swc_ecma_codegen::to_code(&prog)))
+        }
+        _ => Ok(v.into()),
     }
-
-    let mut prog = parser
-        .parse_program()
-        .map_err(|mut e| {
-            // Unrecoverable fatal error occurred
-            e.into_diagnostic(&handler).emit()
-        })
-        .expect("failed to parser module");
-
-    prog.visit_mut_with(&mut weevy_swc_core::SourceMapper {
-        sm: cm.clone(),
-        data: Default::default(),
-        id: Ident::new(
-            Atom::new(format!(
-                "__Weevy_{}_Inject",
-                base64::engine::general_purpose::STANDARD.encode(q2)
-            )),
-            prog.span(),
-            SyntaxContext::empty(),
-        ),
-        root: SyntaxContext::empty(),
-        source_mapper: default_source_mapper(&prog, SyntaxContext::empty()),
-    });
-    prog.visit_mut_with(&mut weevy_swc_core::Wimple {
-        root: SyntaxContext::empty(),
-        guest_id: Atom::new("MainContent"),
-    });
-
-    Ok(Body::new(swc_ecma_codegen::to_code(&prog)))
 }
